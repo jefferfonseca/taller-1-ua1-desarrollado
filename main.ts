dc930b2f23cc9c844a550c375a6bcf2f1a25861b
jefferfonseca <63508641+jefferfonseca@users.noreply.github.com>
2021-05-12 22:11:28 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,5 +1,7 @@
 let Conductividad = 0
 let PConduct = 0
+let Humedad = 0
+let PHumedad = 0
 basic.forever(function () {
     Conductividad = pins.analogReadPin(AnalogPin.P0)
     PConduct = pins.map(
@@ -7,13 +9,200 @@ basic.forever(function () {
     0,
     1023,
     0,
-    4
+    5
     )
-    if (PConduct >= 0 && PConduct <= 1) {
-        for (let index = 4; index > 4; index--) {
-            led.plot(0, index)
-        }
+    if (PConduct == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (PConduct > 0 && PConduct <= 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
     } else {
-    	
+        if (PConduct > 1 && PConduct <= 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                # . . . .
+                `)
+        } else {
+            if (PConduct > 2 && PConduct <= 3) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    # . . . .
+                    # . . . .
+                    # . . . .
+                    `)
+            } else {
+                if (PConduct > 3 && PConduct <= 4) {
+                    basic.showLeds(`
+                        . . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        `)
+                } else {
+                    if (PConduct > 4 && PConduct <= 5) {
+                        basic.showLeds(`
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            `)
+                    }
+                }
+            }
+        }
+    }
+})
+basic.forever(function () {
+    Humedad = pins.analogReadPin(AnalogPin.P1)
+    PHumedad = pins.map(
+    Humedad,
+    0,
+    1023,
+    0,
+    5
+    )
+    if (PHumedad == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (PHumedad > 0 && PHumedad <= 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+        if (PHumedad > 1 && PHumedad <= 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                # . . . .
+                `)
+        } else {
+            if (PHumedad > 2 && PHumedad <= 3) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    # . . . .
+                    # . . . .
+                    # . . . .
+                    `)
+            } else {
+                if (PHumedad > 3 && PHumedad <= 4) {
+                    basic.showLeds(`
+                        . . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        `)
+                } else {
+                    if (PHumedad > 4 && PHumedad <= 5) {
+                        basic.showLeds(`
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            `)
+                    }
+                }
+            }
+        }
+    }
+})
+basic.forever(function () {
+    Humedad = pins.analogReadPin(AnalogPin.P1)
+    PHumedad = pins.map(
+    Humedad,
+    0,
+    1023,
+    0,
+    5
+    )
+    if (PHumedad == 0) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            `)
+    }
+    if (PHumedad > 0 && PHumedad <= 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . .
+            `)
+    } else {
+        if (PHumedad > 1 && PHumedad <= 2) {
+            basic.showLeds(`
+                . . . . .
+                . . . . .
+                . . . . .
+                # . . . .
+                # . . . .
+                `)
+        } else {
+            if (PHumedad > 2 && PHumedad <= 3) {
+                basic.showLeds(`
+                    . . . . .
+                    . . . . .
+                    # . . . .
+                    # . . . .
+                    # . . . .
+                    `)
+            } else {
+                if (PHumedad > 3 && PHumedad <= 4) {
+                    basic.showLeds(`
+                        . . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        # . . . .
+                        `)
+                } else {
+                    if (PHumedad > 4 && PHumedad <= 5) {
+                        basic.showLeds(`
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            # . . . .
+                            `)
+                    }
+                }
+            }
+        }
     }
 })
