53bc4f78510b112031bef7e63572db94786281c4
jefferfonseca <63508641+jefferfonseca@users.noreply.github.com>
2021-05-13 06:48:43 -0500

    Actualizar pxt.json, main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,65 +1,9 @@
-let Acidez = 0
-let PAcidez = 0
 let Conductividad = 0
 let PConduct = 0
+let Acidez = 0
+let PAcidez = 0
 let Humedad = 0
 let PHumedad = 0
-basic.forever(function () {
-    Acidez = pins.analogReadPin(AnalogPin.P1)
-    PAcidez = pins.map(
-    Acidez,
-    0,
-    1023,
-    0,
-    5
-    )
-    if (PAcidez == 0) {
-        led.unplot(1, 0)
-        led.unplot(1, 1)
-        led.unplot(1, 2)
-        led.unplot(1, 3)
-        led.unplot(1, 4)
-    }
-    if (PAcidez > 0 && PAcidez <= 1) {
-        led.unplot(1, 0)
-        led.unplot(1, 1)
-        led.unplot(1, 2)
-        led.unplot(1, 3)
-        led.plot(1, 4)
-    } else {
-        if (PAcidez > 1 && PAcidez <= 2) {
-            led.unplot(1, 0)
-            led.unplot(1, 1)
-            led.unplot(1, 2)
-            led.plot(1, 3)
-            led.plot(1, 4)
-        } else {
-            if (PAcidez > 2 && PAcidez <= 3) {
-                led.unplot(1, 0)
-                led.unplot(1, 1)
-                led.plot(1, 2)
-                led.plot(1, 3)
-                led.plot(1, 4)
-            } else {
-                if (PAcidez > 3 && PAcidez <= 4) {
-                    led.unplot(1, 0)
-                    led.plot(1, 1)
-                    led.plot(1, 2)
-                    led.plot(1, 3)
-                    led.plot(1, 4)
-                } else {
-                    if (PAcidez > 4 && PAcidez <= 5) {
-                        led.plot(1, 0)
-                        led.plot(1, 1)
-                        led.plot(1, 2)
-                        led.plot(1, 3)
-                        led.plot(1, 4)
-                    }
-                }
-            }
-        }
-    }
-})
 basic.forever(function () {
     Conductividad = pins.analogReadPin(AnalogPin.P0)
     PConduct = pins.map(
@@ -118,6 +62,62 @@ basic.forever(function () {
     }
 })
 basic.forever(function () {
+    Acidez = pins.analogReadPin(AnalogPin.P1)
+    PAcidez = pins.map(
+    Acidez,
+    0,
+    1023,
+    0,
+    5
+    )
+    if (PAcidez == 0) {
+        led.unplot(2, 0)
+        led.unplot(2, 1)
+        led.unplot(2, 2)
+        led.unplot(2, 3)
+        led.unplot(2, 4)
+    }
+    if (PAcidez > 0 && PAcidez <= 1) {
+        led.unplot(2, 0)
+        led.unplot(2, 1)
+        led.unplot(2, 2)
+        led.unplot(2, 3)
+        led.plot(2, 4)
+    } else {
+        if (PAcidez > 1 && PAcidez <= 2) {
+            led.unplot(2, 0)
+            led.unplot(2, 1)
+            led.unplot(2, 2)
+            led.plot(2, 3)
+            led.plot(2, 4)
+        } else {
+            if (PAcidez > 2 && PAcidez <= 3) {
+                led.unplot(2, 0)
+                led.unplot(2, 1)
+                led.plot(2, 2)
+                led.plot(2, 3)
+                led.plot(2, 4)
+            } else {
+                if (PAcidez > 3 && PAcidez <= 4) {
+                    led.unplot(2, 0)
+                    led.plot(2, 1)
+                    led.plot(2, 2)
+                    led.plot(2, 3)
+                    led.plot(2, 4)
+                } else {
+                    if (PAcidez > 4 && PAcidez <= 5) {
+                        led.plot(2, 0)
+                        led.plot(2, 1)
+                        led.plot(2, 2)
+                        led.plot(2, 3)
+                        led.plot(2, 4)
+                    }
+                }
+            }
+        }
+    }
+})
+basic.forever(function () {
     Humedad = pins.analogReadPin(AnalogPin.P2)
     PHumedad = pins.map(
     Humedad,
@@ -127,46 +127,46 @@ basic.forever(function () {
     5
     )
     if (PHumedad == 0) {
-        led.unplot(2, 0)
-        led.unplot(2, 1)
-        led.unplot(2, 2)
-        led.unplot(2, 3)
-        led.unplot(2, 4)
+        led.unplot(4, 0)
+        led.unplot(4, 1)
+        led.unplot(4, 2)
+        led.unplot(4, 3)
+        led.unplot(4, 4)
     }
     if (PHumedad > 0 && PHumedad <= 1) {
-        led.unplot(2, 0)
-        led.unplot(2, 1)
-        led.unplot(2, 2)
-        led.unplot(2, 3)
-        led.plot(2, 4)
+        led.unplot(4, 0)
+        led.unplot(4, 1)
+        led.unplot(4, 2)
+        led.unplot(4, 3)
+        led.plot(4, 4)
     } else {
         if (PHumedad > 1 && PHumedad <= 2) {
-            led.unplot(2, 0)
-            led.unplot(2, 1)
-            led.unplot(2, 2)
-            led.plot(2, 3)
-            led.plot(2, 4)
+            led.unplot(4, 0)
+            led.unplot(4, 1)
+            led.unplot(4, 2)
+            led.plot(4, 3)
+            led.plot(4, 4)
         } else {
             if (PHumedad > 2 && PHumedad <= 3) {
-                led.unplot(2, 0)
-                led.unplot(2, 1)
-                led.plot(2, 2)
-                led.plot(2, 3)
-                led.plot(2, 4)
+                led.unplot(4, 0)
+                led.unplot(4, 1)
+                led.plot(4, 2)
+                led.plot(4, 3)
+                led.plot(4, 4)
             } else {
                 if (PHumedad > 3 && PHumedad <= 4) {
-                    led.unplot(2, 0)
-                    led.plot(2, 1)
-                    led.plot(2, 2)
-                    led.plot(2, 3)
-                    led.plot(2, 4)
+                    led.unplot(4, 0)
+                    led.plot(4, 1)
+                    led.plot(4, 2)
+                    led.plot(4, 3)
+                    led.plot(4, 4)
                 } else {
                     if (PHumedad > 4 && PHumedad <= 5) {
-                        led.plot(2, 0)
-                        led.plot(2, 1)
-                        led.plot(2, 2)
-                        led.plot(2, 3)
-                        led.plot(2, 4)
+                        led.plot(4, 0)
+                        led.plot(4, 1)
+                        led.plot(4, 2)
+                        led.plot(4, 3)
+                        led.plot(4, 4)
                     }
                 }
             }
